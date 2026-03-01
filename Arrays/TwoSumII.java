d4380a4e6d9d088a77c4ea9ccf69a3ccf5b83d4b
archi priya <140436068+archipriya03@users.noreply.github.com>
2026-03-01 19:37:20 +0530

    Update TwoSumII.java
    using 2 pointer

--- Arrays/TwoSumII.java
+++ Arrays/TwoSumII.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int l = 0;
         int r = numbers.length - 1;
-
+//using the 2 pointers
         while (l < r) {
             int sum = numbers[l] + numbers[r];
 
